cb820a6784a684cc5eb0adddd0dbf9e523cff912
Lucas Meira <lucasmeiraprofessional@gmail.com>
2024-03-12 12:07:56 -0300

    Passo 2

--- exercicios.js
+++ exercicios.js
@@ -5,3 +5,15 @@ const saudacao = (nome) =>{
     return `Seja bem vindo(a) ${nome}`;
 }
 console.log(saudacao(pessoa));
+
+const idade = 19;
+
+const verificaIdade = (idade) =>{
+    if (idade < 18) {
+        return `Voce  tem ${idade} anos e é menor de idade`;
+    } else {
+        return `Voce  tem ${idade} anos e é maior de idade`;
+    }
+}
+
+console.log(verificaIdade(idade));
